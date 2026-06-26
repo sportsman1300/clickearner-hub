fef15de70aab18d31d057167a7df3ac487408eb4
sportsman1300 <cosminenegoita@gmail.com>
2026-06-25 20:24:14 -0500

    Add _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,1 @@
+import {Html,Head,Main,NextScript} from 'next/document';export default function Document(){return(<Html lang=\"en\"><Head/><body className=\"bg-dark-bg text-white\"><Main/><NextScript/></body></Html>);}
